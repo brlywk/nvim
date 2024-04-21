fb7053792103e9a05444f0d26f5769714ce85aa1
Christoph Suessmeier <mail@suessmeier.net>
2024-04-21 17:32:47 +0200

    Re-Enabled transparency

--- lua/brlywk/plugins/colorscheme.lua
+++ lua/brlywk/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	opts = {
 		flavour = "mocha",
-		transparent_background = false,
+		transparent_background = true,
 	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
