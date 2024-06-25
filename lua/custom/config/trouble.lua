3d6db311b5f222fc581788cd88582b6078d7e726
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2024-06-25 13:03:50 +0200

    Trouble config updated

--- lua/custom/config/trouble.lua
+++ lua/custom/config/trouble.lua
@@ -1,32 +1,61 @@
------ setup -----
+--- setup -----
 local trouble = require "trouble"
-trouble.setup {}
+trouble.setup {
+    modes = {
+        split_diagnostics = {
+            mode = "diagnostics",
+            preview = {
+                type = "split",
+                relative = "win",
+                position = "right",
+                size = 0.3,
+            },
+        },
+        doc_and_project = {
+            mode = "diagnostics", -- inherit from diagnostics mode
+            filter = {
+                any = {
+                    buf = 0, -- current buffer
+                    {
+                        severity = vim.diagnostic.severity.ERROR, -- errors only
+                        -- limit to files in the current project
+                        function(item)
+                            return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                        end,
+                    },
+                },
+            },
+        },
+        cascade = {
+            mode = "diagnostics", -- inherit from diagnostics mode
+            filter = function(items)
+                local severity = vim.diagnostic.severity.HINT
+                for _, item in ipairs(items) do
+                    severity = math.min(severity, item.severity)
+                end
+                return vim.tbl_filter(function(item)
+                    return item.severity == severity
+                end, items)
+            end,
+        },
+    },
+}
 
------ keymap -----
+-- keymap -----
 local set = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 opts.desc = "Document diagnostics"
 set("n", "<leader>xd", function()
-    trouble.open "document_diagnostics"
-end, opts)
-
-opts.desc = "Next"
-set("n", "<leader>xn", function()
-    trouble.next()
-end, opts)
-
-opts.desc = "Previous"
-set("n", "<leader>xp", function()
-    trouble.prev()
+    trouble.open { mode = "split_diagnostics", focus = true }
 end, opts)
 
 opts.desc = "Workspace diagnostics"
 set("n", "<leader>xw", function()
-    trouble.open "workspace_diagnostics"
+    trouble.open { mode = "doc_and_project", focus = true }
 end, opts)
 
-opts.desc = "Open"
+opts.desc = "Toggle"
 set("n", "<leader>xx", function()
-    trouble.open()
+    trouble.toggle { mode = "cascade", focus = true }
 end, opts)
