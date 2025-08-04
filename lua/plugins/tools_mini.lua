c7810bb936ca3a59e2f2b1a39bc6630e813c838e
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2025-08-04 09:44:09 +0200

    Color tweaks

--- lua/plugins/tools_mini.lua
+++ lua/plugins/tools_mini.lua
@@ -44,5 +44,13 @@ return {
 
         ---- Extended a/i functionality ----
         require("mini.ai").setup { n_lines = 500 }
+
+        ---- Override some color settings ----
+        local theme_flavour = require("config.plugins").theme_flavour
+        local theme_colors = require("catppuccin.palettes").get_palette(theme_flavour)
+        vim.api.nvim_set_hl(0, "MiniJump", {
+            fg = theme_colors.base,
+            bg = theme_colors.sky,
+        })
     end,
 }
